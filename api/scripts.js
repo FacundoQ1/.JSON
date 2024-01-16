27496ecd61b0916e56c23f5fa4fa0a692f6b4d50
FacundoQ1 <45350299facu@gmail.com>
2024-01-16 18:01:40 -0300

    server add

--- api/scripts.js
+++ api/scripts.js
@@ -1,23 +1,83 @@
-function enviarFormulario(event) {
-    event.preventDefault();
-  
+document.addEventListener("DOMContentLoaded", () => {
+
+  function enviarFormulario() {
     var formulario = document.getElementById("miFormulario");
-    var datos = new FormData(formulario);
   
-    fetch('/guardarDatos', {
-      method: 'POST',
-      body: datos,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded', // Ajusta según sea necesario
-      },
+    // Obtener los valores de los campos
+    var nombre = document.getElementById("nombrecito").value;
+    var edad = document.getElementById("edadcita").value;
+    var correo = document.getElementById("correito").value;
+  
+    // Crear un objeto con los datos del formulario
+    var datos = {
+        nombre: nombre,
+        edad: edad,
+        correo: correo
+    };
+    console.log(datos);
+
+    // Enviar los datos al servidor
+    fetch('http://localhost:3000/guardarDatos', {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers: {
+            'Content-Type': 'application/json'
+        },
     })
     .then(response => response.json())
     .then(data => {
-      console.log('Datos guardados:', data);
-      // Puedes realizar acciones adicionales después de guardar los datos
+        console.log('Datos guardados:', data);
+        // Puedes realizar acciones adicionales después de guardar los datos
     })
     .catch(error => {
-      console.error('Error al guardar los datos:', error);
+        console.error('Error al guardar los datos:', error);
     });
   }
-  
+
+  const enviarDatos = document.getElementById("enviarDatos");
+
+  enviarDatos.addEventListener("click", (event) => {
+    event.preventDefault();
+    enviarFormulario();
+  });
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  const buscarita = document.getElementById("buscarita");
+
+  buscarita.addEventListener("click", (event) => {
+    event.preventDefault();
+    const correo = document.getElementById('inputCorreo').value;
+
+    // Verifica si el campo de correo no está vacío antes de hacer la solicitud
+    if (correo.trim() !== '') {
+      // Realiza la solicitud al servidor para buscar usuarios por correo
+      fetch(`http://localhost:3000/buscarUsuarios?correo=${correo}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        const usuarioEncontrado = data.usuarios[0]; // Suponemos que solo se encuentra un usuario
+
+        // Muestra la información en el HTML
+        if (usuarioEncontrado) {
+          document.getElementById('nombre').textContent = usuarioEncontrado.nombre;
+          document.getElementById('edad').textContent = usuarioEncontrado.edad;
+          document.getElementById('correo').textContent = usuarioEncontrado.correo;
+        } else {
+          console.log('Usuario no encontrado');
+          // Puedes mostrar un mensaje o realizar otras acciones si el usuario no es encontrado
+        }
+      })
+      .catch(error => {
+        console.error('Error al buscar usuarios:', error);
+      });
+    } else {
+      console.log('Por favor, ingrese un correo válido.');
+    }
+  });
+
+});
--- api/server.js
+++ api/server.js
@@ -1,37 +1,69 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// Cross origin
+app.use(cors({ origin: '*' }));
 
-const uri = 'mongodb+srv://lucascristianlol:45350299facu@cluster0.adjnftl.mongodb.net/?retryWrites=true&w=majority';
 
-app.post('/guardarDatos', (req, res) => {
-  const datos = req.body;
 
-  MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) throw err;
 
-    // Selecciona la colección
-    const collection = client.db('basededatosdepelusa').collection('users');
+const uri = 'mongodb+srv://9zPelusa:45350299@pelusita.9pedvg3.mongodb.net/database-saillen';
 
-    // Inserta los datos en la colección
-    collection.insertOne(datos, (err, result) => {
-      if (err) throw err;
+// Endpoint para manejar la solicitud POST
+app.post('/guardarDatos', async (req, res) => {
+  try {
+    const datos = req.body;
 
-      console.log('Datos guardados en MongoDB Atlas');
-      client.close();
+    // Conectar a la base de datos
+    const client = new MongoClient(uri);
+    await client.connect();
 
-      // Envía una respuesta al cliente (puede ser personalizado según tus necesidades)
-      res.json({ mensaje: 'Datos guardados en MongoDB Atlas' });
-    });
-  });
+    // Acceder a la colección y guardar los datos
+    const collection = client.db('SAILLEN').collection('produccion');
+    const result = await collection.insertOne(datos);
+
+    // Cerrar la conexión a la base de datos
+    client.close();
+
+    // Enviar una respuesta al cliente
+    res.json({ mensaje: 'Datos guardados en MongoDB Atlas', id: result.insertedId });
+  } catch (error) {
+    console.error('Error al procesar la solicitud:', error);
+    res.status(500).json({ mensaje: 'Error al procesar la solicitud' });
+  }
 });
 
-app.listen(port, () => {
+app.get('/buscarUsuarios', async (req, res) => {
+  try {
+    const { correo } = req.query;
+
+    // Conectar a la base de datos
+    const client = new MongoClient(uri);
+    await client.connect();
+
+    // Acceder a la colección y buscar usuarios por correo electrónico
+    const collection = client.db('SAILLEN').collection('produccion');
+    const usuarios = await collection.find({ correo }).toArray();
+
+    // Cerrar la conexiÃ³n a la base de datos
+    client.close();
+
+    // Enviar la lista de usuarios encontrados al cliente
+    res.json({ usuarios });
+  } catch (error) {
+    console.error('Error al procesar la solicitud:', error);
+    res.status(500).json({ mensaje: 'Error al procesar la solicitud' });
+  }
+});
+
+
+app.listen(port,() => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
